537cb6d1c49242e692a25b04a6d7bb253fb238b6
Fabian Chagua <chaguamartinkevin@gmail.com>
2022-11-05 20:14:11 -0500

    algunos cambios más

--- serpientita.py
+++ serpientita.py
@@ -1,2 +1,19 @@
 import pygame, sys,time, random
+#importamos librerias
+
 #speed velocidad
+speed= 15
+#tamaño de la ventana
+tamaño_ancho=720
+tamaño_alto=480
+
+
+check_errors = pygame.init()
+if(check_errors[1]>0):
+    print("Error "+ check_errors[1])
+else:
+    print("Iniciando juego Snake ")  
+    
+#Se abre la ventana del juego
+
+   
